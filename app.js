f4bc40e21c92f8cb60910053979359e040a4106a
Alina <tahera.firdose@gmail.com>
2021-10-03 19:12:39 +0530

    new

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static('public'));
-// app.use(express.static('images'));
+// app.use('/static', express.static('public'));
 
 // app.use(express.static(path.join(__dirname, 'public')));
 
--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.set('view engine','ejs')
 app.use(express.static('public'));
 app.use(express.static('images'));
 
-
 const indexRouter = require('./routes')
 app.use('/', indexRouter)
 
