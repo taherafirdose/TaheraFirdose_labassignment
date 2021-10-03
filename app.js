6b5983c0356a87a3058113caee51cda8ffa26e21
Alina <tahera.firdose@gmail.com>
2021-10-03 19:06:18 +0530

    portfolio

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('public'));
-app.use(express.static('images'));
+// app.use(express.static('public'));
+// app.use(express.static('images'));
 
 // app.use(express.static(path.join(__dirname, 'public')));
 
