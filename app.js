3d14cce9bb7dc3bcf81d2e86f894dc963d4ec686
Alina <tahera.firdose@gmail.com>
2021-10-03 19:20:48 +0530

    new file

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use('/static', express.static('public'));
-
+app.use(express.static('public'));
+app.use(express.static('images'));
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
